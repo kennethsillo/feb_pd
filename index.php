241266299890d35a1c04a58ecd9a94381bbd5454
Johnroy Policarpio <policarpiojohnroy@yahoo.com.ph>
2019-10-18 14:15:57 +0800

    index-roy

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Quiz In IT CO-OP | Home</title>
+  <link rel="stylesheet" href="vendor/css/bootstrap.css">
+  </head>
+    <nav class="navbar navbar-light bg-dark justify-content-between">
+      <a class="navbar-brand text-light" href="index.php">Quiz In It</a>
+        <form class="form-inline">
+            <button class="btn btn-primary my-2 my-sm-0" type="submit">Login</button>
+            <button class="btn btn-danger my-2 my-sm-0 ml-2" type="submit">Register</button>
+        </form>
+    </nav>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+        <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom">
+  <div class="container text-center">
+    <small>Copyright &copy; Your Website</small>
+  </div>
+</footer>
