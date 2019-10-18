4ab9815b99efb1a9239201f6e6fe614c528b2cb8
Johnroy Policarpio <policarpiojohnroy@yahoo.com.ph>
2019-10-18 15:27:29 +0800

    index-roy

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Quiz In IT CO-OP | Home</title>
+  <link rel="stylesheet" href="vendor/css/bootstrap.css">
+  <link rel="stylesheet" href="resources/styles/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-dark ">
+      <a class="navbar-brand text-light" href="index.php">Return To Home</a>
+    </nav>
+
+        <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom">
+  <div class="container text-center">
+    <small>Copyright &copy; Your Website</small>
+  </div>
+</footer>
+  </body>
